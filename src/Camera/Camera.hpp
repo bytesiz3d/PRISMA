25bba6f057e43346a6f153d587d62c5cdfc35fce
onecalled3bkr <boudy999@gmail.com>
2019-11-23 15:30:31 +0200

    Implemented the camera class

--- src/Camera/Camera.hpp
+++ src/Camera/Camera.hpp
@@ -1,5 +1,48 @@
 #pragma once
-#include <glm/vec3.hpp>
-#include <glm/vec2.hpp>
+#include <cmath>
 
-class Camera;
+#include <glm/common.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/scalar_constants.hpp>
+
+class Camera
+{
+    float yaw;
+    float pitch;
+
+    float yawSens;
+    float pitchSens;
+    float movementSens;
+
+    glm::vec3 direction;
+    glm::vec3 up;
+
+    float perspectiveFoVy;
+    float aspectRatio;
+
+    float near;
+    float far;
+
+    const float PI;
+
+    glm::mat4 ViewMatrix();
+    glm::mat4 ProjectionMatrix();
+
+public:
+    glm::vec3 position;
+
+    Camera();
+
+    void UpdateCamera(glm::vec2 mouseDelta, glm::vec3 movement);
+
+    glm::mat4 ViewProjectionMatrix();
+
+    void SetTarget(glm::vec3 target);
+
+    glm::vec3 right();
+};
+
