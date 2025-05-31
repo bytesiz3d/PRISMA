cf1ce076d59830a0d80091a0fb7a08d574abf3dd
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2025-05-31 15:31:01 +0300

    Fix Input Manager Adding/Removing

--- src/input/input_manager.cpp
+++ src/input/input_manager.cpp
@@ -4,14 +4,25 @@ bool InputManager::compare(const UserInput* a, const UserInput* b) {
     return *a < *b;
 }
 
-std::set<const UserInput*, decltype(InputManager::compare)*> InputManager::set(&InputManager::compare);
+InputManager::SetType InputManager::set(&InputManager::compare);
+std::unordered_map<const UserInput*, InputManager::SetType::iterator> InputManager::inputMap;
 
 void InputManager::add(const UserInput* input) {
-    set.insert(input);
+    if (!inputMap.contains(input)) {
+        // If the input is not already in the set, insert it
+        inputMap[input] = set.insert(input);
+    }
 }
 
 void InputManager::remove(const UserInput* input) {
-    set.erase(input);
+    if (!inputMap.contains(input)) {
+        // If the input is not in the map, nothing to remove
+        return;
+    }
+    // Remove the input from the set and the map
+    const auto it = inputMap[input];
+    inputMap.erase(input);
+    set.erase(it);
 }
 
 GameInputData InputManager::ProcessGameInput() {
--- src/input/input_manager.hpp
+++ src/input/input_manager.hpp
@@ -2,12 +2,17 @@
 #define INPUT_HPP
 
 #include <set>
+#include <unordered_map>
 #include "user_input.hpp"
 
 
 class InputManager {
     static bool compare(const UserInput* a, const UserInput* b);
-    static std::set<const UserInput*, decltype(compare)*> set;
+
+    typedef std::multiset<const UserInput*, decltype(compare)*> SetType;
+
+    static SetType set;
+    static std::unordered_map<const UserInput*, SetType::iterator> inputMap;
 public:
     static void add(const UserInput* input);
     static void remove(const UserInput* input);
